bdd7471c014c3b3a5ca283a037cc5ab05e1ecbd7
unicorn-84 <unicorn-84@ya.ru>
2024-02-21 01:08:46 +0300

    chore: change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Raleway Variable"', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [require('@tailwindcss/forms'), require('daisyui')],
   daisyui: {
