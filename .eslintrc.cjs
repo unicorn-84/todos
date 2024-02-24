f0f4b0ab169584948d12510f1d9463550ee879a9
unicorn-84 <unicorn-84@ya.ru>
2024-02-24 23:50:36 +0300

    chore(prettier): change prettierignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['standard-with-typescript', 'plugin:react/recommended', 'plugin:jest-dom/recommended', 'plugin:testing-library/react', 'plugin:jsx-a11y/recommended'],
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:testing-library/react',
+    'plugin:jsx-a11y/recommended',
+  ],
   ignorePatterns: ['dist'],
   overrides: [
     {
@@ -23,11 +29,7 @@ module.exports = {
   plugins: ['react'],
   rules: {
     semi: 'off',
-    '@typescript-eslint/semi': [
-      'error',
-      'always',
-      { omitLastInOneLineBlock: true },
-    ],
+    '@typescript-eslint/semi': ['error', 'always', { omitLastInOneLineBlock: true }],
     'comma-dangle': 'off',
     '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
     'no-console': ['error', { allow: ['error'] }],
