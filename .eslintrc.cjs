570c6c8843e3be78f7df03352df707feaf72a055
unicorn-84 <unicorn-84@ya.ru>
2024-02-29 21:30:54 +0300

    perf: format with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,11 @@ module.exports = {
   plugins: ['react'],
   rules: {
     semi: 'off',
-    '@typescript-eslint/semi': ['error', 'always', { omitLastInOneLineBlock: true }],
+    '@typescript-eslint/semi': [
+      'error',
+      'always',
+      { omitLastInOneLineBlock: true },
+    ],
     'comma-dangle': 'off',
     '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
     'no-console': ['error', { allow: ['error'] }],
